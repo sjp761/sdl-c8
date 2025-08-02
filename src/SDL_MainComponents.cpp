f8a08d5a1a1790a74a42c438fdfd0f8a08e43da5
Simon P <171447425+sjp761@users.noreply.github.com>
2025-08-02 16:06:15 -0500

    Remove unused code

--- src/SDL_MainComponents.cpp
+++ src/SDL_MainComponents.cpp
@@ -16,15 +16,4 @@ void SDL_MainComponents::init()
 {
     window = SDL_CreateWindow("SDL Window", configuration::WINDOW_WIDTH * configuration::SCALE_FACTOR, configuration::WINDOW_HEIGHT * configuration::SCALE_FACTOR, SDL_WINDOW_RESIZABLE);
     renderer = SDL_CreateRenderer(window, nullptr);
-    auto [r, g, b, a] = extractRGBA();
-    SDL_SetRenderDrawColor(renderer, r, g, b, a); // Set draw color to yellow
-}
-
-std::tuple<uint8_t, uint8_t, uint8_t, uint8_t> SDL_MainComponents::extractRGBA()
-{
-    uint8_t r = (configuration::DEFAULT_COLOR >> 24) & 0xFF; // Shifts the bits by 24 then only grabs the first 8 bits to get the color
-    uint8_t g = (configuration::DEFAULT_COLOR >> 16) & 0xFF;
-    uint8_t b = (configuration::DEFAULT_COLOR >> 8) & 0xFF;
-    uint8_t a = configuration::DEFAULT_COLOR & 0xFF;
-    return std::make_tuple(r, g, b, a);
 }
